d0f581b61f307d589a34dc4ddb09f2c9cdf38c5d
Albert <official1908teamfbi@gmail.com>
2022-05-27 07:44:03 +0300

    hehe

--- js/demo.js
+++ js/demo.js
@@ -1,36 +1,117 @@
-//Создаем объект предприятия
-let itCompany = new Company("500na700");
-let isMainMenuActive = false;
+//-----События нажатия на элементы меню------
+let dialogueInput = document.querySelector(".dialogue_input");
+let dialogueForm = document.querySelector(".dialogue__form");
+let dialogueOutput = document.querySelector(".dialogue_output");
+let answer;
+let currentPlan = null;
 
-function mainMenu(){
-  switch(prompt(`IT-Предприятие: ${itCompany.getName}\n1.Вывести название предприятия\n2.Поменять название предприятия\n3.Добавить отдел\n4.Удалить первый отдел\n5.Посчитать сумму финансов\n6.Вывести всю информацию\n7.Выбрать отдел\n8.Выход`)){
-    case "1":
-      alert(itCompany.getName);
-      break;
-    case "2":
-      itCompany.setName = prompt("Введите новое название");
-      break;
-    case "3":
-      itCompany.addDepartment(prompt("Введите название нового отдела"));
-      break;
-    case "4":
-      itCompany.deleteDepartment();
-      break;
-    case "5":
-      alert(itCompany.countFinance());
-      break;
-    case "6":
-      alert(itCompany.getInfo);
-      break;
-    case "7":
-      itCompany.chooseDepartment(prompt("Введите название отдела"));
-      break;
-    case "8":
-      isMainMenuActive = false;
-      break;
-    default:
-      alert("Введите верное значение");
-      break;
-  }
+//Асинхронность
+function submit(callback){
+  dialogueForm.onsubmit = () => callback(answer); 
 }
-while(isMainMenuActive){mainMenu()}
+
+//Данные формы ввода
+dialogueForm.addEventListener("submit", e => {
+  e.preventDefault();     
+  let formData = new FormData(dialogueForm);
+  if (formData.get("answer") != ""){
+    dialogueInput.classList.remove("_active");
+    answer = formData.get("answer");
+    dialogueForm.querySelector(".input").value = "";
+  }
+});
+
+//Создание объекта предприятия
+let tableCompanyTitle = document.querySelector(".tabele_company th");
+let itCompany;
+submit(answer => itCompany = new Company(answer));
+
+//1.Вывести название преприятия
+function getName() {
+  dialogueOutput.classList.add("_active");
+  dialogueOutput.lastElementChild.lastElementChild.textContent = itCompany.getName;
+}
+
+//2.Изменить название предприятия
+function setName() {
+  dialogueInput.querySelector(".label").textContent = "Введите новое название предприятия: ";
+  dialogueInput.classList.add("_active");
+  submit(answer => itCompany.setName = answer);
+}
+
+//3.Добавить отдел
+function addDepartment() {
+  dialogueInput.querySelector(".label").textContent = "Введите название отдела: ";
+  dialogueInput.classList.add("_active");
+  submit(answer => itCompany.addDepartment = answer);
+}
+
+//4.Удалить отдел
+function deleteDepartment() {
+  dialogueOutput.classList.add("_active");
+  dialogueOutput.lastElementChild.lastElementChild.textContent = itCompany.deleteDepartment();
+}
+
+//5.Найти отдел
+function searchDepartment() {
+  alert(itCompany.searchDepartment(prompt("Введите название плана")));
+}
+
+//7.Выбрать план
+function chooseDepartment() {
+  itCompany.chooseDepartment();
+}
+
+//Кнопка закрыть
+let closeBtn = document.querySelectorAll("hr.close");
+closeBtn.forEach(el => {
+  el.onclick = () => {
+    let closedWindow = el.parentElement.parentElement.parentElement;
+    closedWindow.classList.remove("_active");
+    if (closedWindow.classList.contains("plan")){
+      closedWindow.querySelector("select").remove();
+    }
+  }
+});
+
+let fullscreenBtn = document.querySelectorAll("hr.fullscreen");
+fullscreenBtn.forEach(el => {
+  el.onclick = () => {
+    el.parentElement.parentElement.parentElement.classList.toggle("_fullscreen");
+  }
+});
+
+//-----События нажатия на элементы меню------
+//1.Вывести название плана
+function showPlanName() {
+  alert(currentPlan.getName);
+}
+
+//2.Изменить название плана
+function changePlanName() {
+  currentPlan.setName = prompt("Введите новое название:");
+}
+
+//3.Вывести предмет
+function showPlanHours() {
+  alert(currentPlan.getHours(prompt("Введите название предмета:")));
+}
+
+//4.Добавить предмет и часы
+function addPlanHours() {
+  currentPlan.addHours(prompt("Введите название предмета:"), prompt("Введите количество часов:")); }
+
+//5.Изменить часы
+function changePlanHours() {
+  currentPlan.changeHours(prompt("Введите название предмета:"), prompt("Введите новое значение:"));
+}
+
+//6.Сумма
+function showPlanSum() {
+  alert(currentPlan.getHoursSum);
+}
+
+//7.Показать всю информацию
+function showPlanData() {
+  alert(currentPlan.getData);
+}
